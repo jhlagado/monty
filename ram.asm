ad337c7fa1a13ffa61b24158f8946b21545b909f
John Hardy <jh@lagado.com>
2023-05-08 23:58:49 +1000

    first

---
+++ ram.asm
@@ -0,0 +1,45 @@
+.ORG RAMSTART
+
+            ds DSIZE
+STACK:
+
+tbPtr:      ds 2                ; reserved for tests
+
+RST08:      ds 2                 
+RST10:      ds 2                 
+RST18:      ds 2                 
+RST20:      ds 2                 
+RST28:      ds 2                 
+RST30:      ds 2                ; 
+BAUD        ds 2                ; 
+INTVEC:     ds 2                ; 
+NMIVEC:     ds 2                ; 
+GETCVEC:    ds 2                ;   
+PUTCVEC:    ds 2                ;   
+
+sysVars:
+
+vFrac:      ds 2                ; initialised sys variables
+vDataWidth: ds 2                ; 
+vTIBPtr:    ds 2                ; 
+vPointer    ds 2                ; 
+vLastDef:   ds 2                ; 
+vHashStr:   ds 2                ; 
+vNext       ds 2                ; 
+vHeapPtr:   ds 2                ; 
+
+vTemp1:     ds 2                ; uninitialised sys variables
+
+.align $100
+TIB:        ds TIBSIZE
+
+.align $100
+pad:        ds $100
+
+.align $100
+hashSlots:  ds $100
+
+.align $100
+hashWords:  ds $100
+
+HEAP:         
---
+++ test.fast.mac.asm
@@ -0,0 +1,51 @@
+.macro expect,msg1,val1
+
+    pop HL
+    push HL
+    ld DE,val1
+    or A
+    sbc HL,DE
+    ld A,L
+    or H
+    jr Z,expect%%M
+
+    call printStr
+    .cstr "Code: ",msg1
+
+    call printStr
+    .cstr "\r\n\r\nExpected: "
+    ld HL,val1
+    call prtdec
+
+    call printStr
+    .cstr "\r\n\r\nActual: "
+    pop hl
+    push hl
+    call prtdec
+    pop hl
+    call printStr
+    .cstr " (#"
+    call prthex
+    call printStr
+    .cstr ")\r\n"
+
+    halt
+    .cstr
+expect%%M:
+    pop HL
+.endm
+
+.macro test,code1,val1
+    ld SP,STACK
+    call init
+    call execStr
+    .cstr code1
+    expect code1,val1
+.endm
+
+.macro kall,label1
+    db msb(label1 / 2) + $80
+    db lsb(label1 / 2)
+.endm
+
+
